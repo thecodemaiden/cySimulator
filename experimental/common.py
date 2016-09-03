3fd06cf8e4911e3b958e6853b38c43c1290a2692
Adeola Bannis <thecodemaiden@gmail.com>
2016-09-03 16:56:20 -0400

    Getting rid of extra cruft

--- experimental/common.py
+++
@@ -1,4 +0,0 @@
-from collections import namedtuple
-
-Rect3d = namedtuple('Rect3d', ['centerX', 'centerY', 'centerZ', 'xLength', 'yLength', 'zLength'])
-Point3d = namedtuple('Point3d', ['x', 'y','z'])
